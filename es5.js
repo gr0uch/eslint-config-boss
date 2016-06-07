ffe1307c568b695d3575beee302e09c74067627e
0x8890 <0x8890@airmail.cc>
2016-06-07 15:53:38 +0200

    disable prefer in es5

--- es5.js
+++ es5.js
@@ -6,6 +6,8 @@ delete config.ecmaFeatures.modules
 config.rules['func-names'] = 0
 config.rules['no-var'] = 0
 config.rules['one-var'] = 0
+config.rules['prefer-const'] = 0
+config.rules['prefer-template'] = 0
 config.rules['strict'] = [ 2, 'global' ]
 
 module.exports = config
